d2d3be6015ca1d508b4d93e2d5f4ff3cfc817a20
Maxime Bailly <maxime3510@gmail.com>
2020-11-24 18:34:33 +0100

    [MODS][AutoHarvest] Clear code & add more check

--- src/app/core/mods/autoHarvest/autoHarvest.ts
+++ src/app/core/mods/autoHarvest/autoHarvest.ts
@@ -2,13 +2,16 @@ import { Logger } from "app/core/electron/logger.helper";
 import { Mod } from "../mod";
 
 export class AutoHarvest extends Mod {
+    private readonly STATE_USE: number = 2;
+
     private waitingInteractives: Map<number, InteractiveElement> = new Map();
     private usedInteractives: Map<number, InteractiveElement> = new Map();
-    private skillsToUse: number[] = [6,39,40];
+    private skillsToUse: number[] = [];
 
     private checkInterval: any;
+    private delayedUseInteractive: ReturnType<typeof setTimeout>;
     private refresh: number = 3000;
-    private isInHarvest: boolean = false;
+    private isInHarvest: {status: boolean, startTime: number, elemId: number} = {status: false, startTime: -1, elemId: 0}; // add elemId
     private maxWeightInPercent: number = 95;
     private weightLimitReached: boolean = false;
 
@@ -18,13 +21,16 @@ export class AutoHarvest extends Mod {
         if(/*this.params.auto_harvest*/ true) {
             Logger.info('- enable AutoHarvest');
 
-            //this.skillsToUse = [6,39,40];
+            setTimeout(() => {
+                this.getJobsSkills();
+            }, 4000);
 
             // Start Listener
             this.onGameMapChange();
             this.onInteractiveElementUpdate();
             this.onInteractiveUseEndedMessage();
             this.onInventoryWeightMessage();
+            this.onStatedElementUpdatedMessage()
 
             this.checkForAwaitingInteractives();
         }
@@ -32,25 +38,47 @@ export class AutoHarvest extends Mod {
 
     private checkForAwaitingInteractives() {
         this.checkInterval = setInterval(() => {
-            if (this.waitingInteractives.size > 0 && !this.isInHarvest && !this.weightLimitReached) {
+            // Reset harvest
+            if (this.isInHarvest.status && (new Date().getTime() - this.isInHarvest.startTime) >= 20000) {
+
+                this.deleteUseInteractiveAtEnd(this.isInHarvest.elemId);
+                console.log('Force "isInHarvest" reinitialization -> ', this.isInHarvest);
+            }
+
+            // Triggers the use of interactive
+            if (this.waitingInteractives.size > 0 && this.checkAllCondition()) {
 
                 let interactive: InteractiveElement = this.waitingInteractives.entries().next().value[1];
                 this.useInteractiveElement(interactive);
                 this.usedInteractives.set(interactive.elementId, interactive);
                 this.waitingInteractives.delete(interactive.elementId);
-
             }
         }, this.refresh);
     }
 
-    private useInteractiveElement(interactive: InteractiveElement) {
-        if (!this.isInHarvest) {
-            this.isInHarvest = true;
-            console.log('UseInteractive -> elementId : ', interactive.elementId);
+    // TODO Move in autoHarvest gestionnaire
+    private getJobsSkills() {
+        const listJobs: Array<number> = this.wGame.gui.playerData.jobs.jobOriginalOrder;
+        listJobs.forEach(jobId => {
+            const job = this.wGame.gui.playerData.jobs.list[jobId];
 
-            setTimeout(() => {
-                this.wGame.isoEngine.useInteractive(interactive.elementId, interactive.skillInstanceUid);   
-            }, this.getRandomTime(1,3));
+            job.description.skills.forEach(skill => {
+                if (skill._type == 'SkillActionDescriptionCollect') {
+                    this.skillsToUse.push(skill.skillId);
+                }
+            });
+        });
+    }
+
+    // TODO Check with wgame if interactive already use
+    private useInteractiveElement(interactive: InteractiveElement) {
+        if (!this.isInHarvest.status) {
+            this.isInHarvest = {status: true, startTime: new Date().getTime(), elemId: interactive.elementId};
+
+            this.delayedUseInteractive = setTimeout(() => {
+                console.log('UseInteractive -> elementId : ', interactive.elementId);
+                this.wGame.isoEngine.queueUseInteractive(interactive.elementId, interactive.skillInstanceUid);   
+            }, this.getRandomTime(2,4));
         }
     }
 
@@ -71,39 +99,35 @@ export class AutoHarvest extends Mod {
         }
     }
 
-    private onGameMapChange() {
-        this.on(this.wGame.dofus.connectionManager, 'MapComplementaryInformationsDataMessage', (e: any) => {
-            try {
-                this.clear();
-                e.interactiveElements.forEach((interactiveElement) => {
-                    this.addInteractiveToWaitingList(interactiveElement);
-                });
-            } catch (error) {
-                Logger.info(error);
-            }
+    private deleteUseInteractiveAtEnd(elemId: number) {
+        if (this.usedInteractives.has(elemId)) {
+            console.log('Delete interactive : ', elemId);
+            this.usedInteractives.delete(elemId);
+            this.isInHarvest = {status: false, startTime: -1, elemId: 0};
+        }
+    }
+
+    private loadInteractiveList(interactiveElements: any) {
+        this.clear();
+        interactiveElements.forEach((interactiveElement) => {
+            this.addInteractiveToWaitingList(interactiveElement);
         });
     }
 
-    private onInteractiveElementUpdate() {
-        this.on(this.wGame.dofus.connectionManager, 'InteractiveElementUpdatedMessage', (e: any) => {
-            try {
-                this.addInteractiveToWaitingList(e.interactiveElement);
-            } catch (error) {
-                Logger.info(error);
-            }
-        });
+    private checkAllCondition(): boolean {
+        return !this.isInHarvest.status                     // If player is already in harvest
+            && !this.weightLimitReached                     // If weight limit reached
+            && !this.wGame.gui.playerData.isFighting;       // If player is in fight
     }
 
-    private onInteractiveUseEndedMessage() {
-        this.on(this.wGame.dofus.connectionManager, 'InteractiveUseEndedMessage', (e: any) => {
-            try {
-                if (this.usedInteractives.has(e.elemId)) {
-                    console.log('Delete interactive : ', e.elemId);
-                    this.usedInteractives.delete(e.elemId);
-                    this.isInHarvest = false;
-                }
-            } catch (error) {
-                Logger.info(error);
+
+    /* Listener function */
+
+    private onStatedElementUpdatedMessage() {
+        this.on(this.wGame.dofus.connectionManager, 'StatedElementUpdatedMessage', (e: any) => {
+            if (this.waitingInteractives.has(e.statedElement.elementId) && e.statedElement.elementState == this.STATE_USE) {
+                console.log('StatedElementUpdatedMessage -> Remove element ' + e.statedElement.elementId + ' already used...');
+                this.waitingInteractives.delete(e.statedElement.elementId);
             }
         });
     }
@@ -113,17 +137,33 @@ export class AutoHarvest extends Mod {
             try {
                 const weightPercent = Math.round(e.weight / e.weightMax * 100);
                 this.weightLimitReached = weightPercent >= this.maxWeightInPercent;
-                if(this.weightLimitReached) console.log('Pods supérieur à ' + this.maxWeightInPercent);
+                if(this.weightLimitReached) console.log('Pods supérieur à ' + this.maxWeightInPercent + '%');
             } catch (error) {
                 Logger.info(error);
             }
         });
     }
 
+    private onInteractiveElementUpdate() {
+        this.on(this.wGame.dofus.connectionManager, 'InteractiveElementUpdatedMessage', (e: any) => this.addInteractiveToWaitingList(e.interactiveElement));
+    }
+
+    private onInteractiveUseEndedMessage() {
+        this.on(this.wGame.dofus.connectionManager, 'InteractiveUseEndedMessage', (e: any) => this.deleteUseInteractiveAtEnd(e.elemId));
+    }
+
+    private onGameMapChange() {
+        this.on(this.wGame.dofus.connectionManager, 'MapComplementaryInformationsDataMessage', (e: any) => this.loadInteractiveList(e.interactiveElements));
+    }
+
+
+    /* Clear and reset function */
+
     private clear() {
         this.waitingInteractives.clear();
         this.usedInteractives.clear();
-        this.isInHarvest = false; // Use to prevent bug -> sometimes onInteractiveUseEndedMessage isn't call
+        if (this.delayedUseInteractive) clearTimeout(this.delayedUseInteractive);
+        this.isInHarvest = {status: false, startTime: -1, elemId: 0};
         console.log('Clear all data');
     }
 
